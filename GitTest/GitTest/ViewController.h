6184ffb39bb57a200510b35b58bdbe30f67fd3c3
hecanlin <hecanlin0731@163.com>
2018-01-11 22:26:57 +0800

    添加项目

---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by applesy on 2018/1/10.
+//  Copyright © 2018年 applesy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by applesy on 2018/1/10.
+//  Copyright © 2018年 applesy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
